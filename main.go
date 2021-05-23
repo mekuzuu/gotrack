ea757b9291c4a828cd2e15e6864b6fd360d70404
nbhdx <nbhd404@gmail.com>
2021-05-23 23:08:34 +0900

    cliで実行できるようにする

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"gotrack/courie/yamato"
 	"gotrack/tablewriter"
 )
 
+var (
+	// Dependency
+	tableWriterOP tablewriter.ITableWriterOperator
+	yamatoOP      yamato.IYamatoOperator
+
+	flagYamato = flag.String("ymt", "", "track shipment transported by Yamato Transport")
+)
+
+func init() {
+	// Dependency
+	tableWriterOP = tablewriter.NewTableWriterOperator(os.Stdout)
+	yamatoOP = yamato.NewYamatoOperator(tableWriterOP)
+}
+
 func main() {
-	tableWriterOP := tablewriter.NewTableWriterOperator(os.Stdout)
-	yamatoOP := yamato.NewYamatoOperator(tableWriterOP)
-	err := yamatoOP.TrackShipments([]string{})
-	if err != nil {
-		panic(err)
+	os.Exit(run())
+}
+
+func run() int {
+	flag.Parse()
+	if *flagYamato != "" {
+		if err := yamatoOP.TrackShipments([]string{}); err != nil {
+			return 1
+		}
 	}
+	return 0
 }
